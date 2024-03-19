2d304d4602cf7fb784441cb82e2d56512b1fe563
samuel212121 <abemmenelik@gmail.com>
2024-03-19 02:50:49 -0700

    remove char

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = ""
-    for char in my_string:
-        if char.lower() != 'c':
-            new_string += char
-            return new_string
+    new_string = my_string.translate({ord(i): None for i in 'cC'})
+    return new_string
