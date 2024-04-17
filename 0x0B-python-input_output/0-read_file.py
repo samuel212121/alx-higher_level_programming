b7579150cdaf18f8e9b3acf83d2f975ecb2bf740
samuel212121 <abemmenelik@gmail.com>
2024-04-17 06:50:45 -0700

    raed file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def read_file(filename=""):
-    filename = "UTF8.txt"
-    with open('UTF8.txt', 'r', encoding="utf-8") as f:
-        read_file = f.read()
+    with open('UTF8', 'r', encoding="utf-8") as f:
+        myread = f.read()
+        read_file('UTF8')
